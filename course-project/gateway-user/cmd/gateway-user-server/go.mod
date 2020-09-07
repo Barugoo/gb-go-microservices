c5217f7a748c3b6679e39ca2a736ef2d61c2275b
Dmitry Geekbrains <barugoo@yandex.ru>
2020-09-07 21:07:53 +0300

    gateway implementation generated with go-swagger

---
+++ course-project/gateway-user/cmd/gateway-user-server/go.mod
@@ -0,0 +1,8 @@
+module github.com/barugoo/gb-go-microservices/course-project/gateway-user/cmd/gateway-user-server
+
+go 1.14
+
+require (
+	github.com/go-openapi/loads v0.19.5
+	github.com/jessevdk/go-flags v1.4.0
+)
---
+++ course-project/gateway-user/restapi/configure_gateway_user.go
@@ -0,0 +1,125 @@
+// This file is safe to edit. Once it exists it will not be overwritten
+
+package restapi
+
+import (
+	"crypto/tls"
+	"net/http"
+
+	"github.com/go-openapi/errors"
+	"github.com/go-openapi/runtime"
+	"github.com/go-openapi/runtime/middleware"
+
+	"github.com/barugoo/gb-go-microservices/course-project/gateway-user/restapi/operations"
+	"github.com/barugoo/gb-go-microservices/course-project/gateway-user/restapi/operations/auth"
+	"github.com/barugoo/gb-go-microservices/course-project/gateway-user/restapi/operations/movies"
+	"github.com/barugoo/gb-go-microservices/course-project/gateway-user/restapi/operations/profile"
+)
+
+//go:generate swagger generate server --target ../../gateway-user --name GatewayUser --spec ../swagger.yaml --principal interface{}
+
+func configureFlags(api *operations.GatewayUserAPI) {
+	// api.CommandLineOptionsGroups = []swag.CommandLineOptionsGroup{ ... }
+}
+
+func configureAPI(api *operations.GatewayUserAPI) http.Handler {
+	// configure the api here
+	api.ServeError = errors.ServeError
+
+	// Set your custom logger if needed. Default one is log.Printf
+	// Expected interface func(string, ...interface{})
+	//
+	// Example:
+	// api.Logger = log.Printf
+
+	api.UseSwaggerUI()
+	// To continue using redoc as your UI, uncomment the following line
+	// api.UseRedoc()
+
+	api.JSONConsumer = runtime.JSONConsumer()
+
+	api.JSONProducer = runtime.JSONProducer()
+
+	// Applies when the "Authorization" header is set
+	if api.BearerAuthAuth == nil {
+		api.BearerAuthAuth = func(token string) (interface{}, error) {
+			return nil, errors.NotImplemented("api key auth (bearerAuth) Authorization from header param [Authorization] has not yet been implemented")
+		}
+	}
+
+	// Set your custom authorizer if needed. Default one is security.Authorized()
+	// Expected interface runtime.Authorizer
+	//
+	// Example:
+	// api.APIAuthorizer = security.Authorized()
+	if api.MoviesGetMovieHandler == nil {
+		api.MoviesGetMovieHandler = movies.GetMovieHandlerFunc(func(params movies.GetMovieParams) middleware.Responder {
+			return middleware.NotImplemented("operation movies.GetMovie has not yet been implemented")
+		})
+	}
+	if api.ProfileGetProfileHandler == nil {
+		api.ProfileGetProfileHandler = profile.GetProfileHandlerFunc(func(params profile.GetProfileParams, principal interface{}) middleware.Responder {
+			return middleware.NotImplemented("operation profile.GetProfile has not yet been implemented")
+		})
+	}
+	if api.MoviesListMoviesHandler == nil {
+		api.MoviesListMoviesHandler = movies.ListMoviesHandlerFunc(func(params movies.ListMoviesParams) middleware.Responder {
+			return middleware.NotImplemented("operation movies.ListMovies has not yet been implemented")
+		})
+	}
+	if api.ProfileListOrdersHandler == nil {
+		api.ProfileListOrdersHandler = profile.ListOrdersHandlerFunc(func(params profile.ListOrdersParams, principal interface{}) middleware.Responder {
+			return middleware.NotImplemented("operation profile.ListOrders has not yet been implemented")
+		})
+	}
+	if api.ProfileListPaymentsHandler == nil {
+		api.ProfileListPaymentsHandler = profile.ListPaymentsHandlerFunc(func(params profile.ListPaymentsParams, principal interface{}) middleware.Responder {
+			return middleware.NotImplemented("operation profile.ListPayments has not yet been implemented")
+		})
+	}
+	if api.AuthLoginUserHandler == nil {
+		api.AuthLoginUserHandler = auth.LoginUserHandlerFunc(func(params auth.LoginUserParams) middleware.Responder {
+			return middleware.NotImplemented("operation auth.LoginUser has not yet been implemented")
+		})
+	}
+	if api.AuthRegisterUserHandler == nil {
+		api.AuthRegisterUserHandler = auth.RegisterUserHandlerFunc(func(params auth.RegisterUserParams) middleware.Responder {
+			return middleware.NotImplemented("operation auth.RegisterUser has not yet been implemented")
+		})
+	}
+	if api.MoviesRentMovieHandler == nil {
+		api.MoviesRentMovieHandler = movies.RentMovieHandlerFunc(func(params movies.RentMovieParams, principal interface{}) middleware.Responder {
+			return middleware.NotImplemented("operation movies.RentMovie has not yet been implemented")
+		})
+	}
+
+	api.PreServerShutdown = func() {}
+
+	api.ServerShutdown = func() {}
+
+	return setupGlobalMiddleware(api.Serve(setupMiddlewares))
+}
+
+// The TLS configuration before HTTPS server starts.
+func configureTLS(tlsConfig *tls.Config) {
+	// Make all necessary changes to the TLS configuration here.
+}
+
+// As soon as server is initialized but not run yet, this function will be called.
+// If you need to modify a config, store server instance to stop it individually later, this is the place.
+// This function can be called multiple times, depending on the number of serving schemes.
+// scheme value will be set accordingly: "http", "https" or "unix"
+func configureServer(s *http.Server, scheme, addr string) {
+}
+
+// The middleware configuration is for the handler executors. These do not apply to the swagger.json document.
+// The middleware executes after routing but before authentication, binding and validation
+func setupMiddlewares(handler http.Handler) http.Handler {
+	return handler
+}
+
+// The middleware configuration happens before anything, this middleware also applies to serving the swagger.json document.
+// So this is a good place to plug in a panic handling middleware, logging and metrics
+func setupGlobalMiddleware(handler http.Handler) http.Handler {
+	return handler
+}
